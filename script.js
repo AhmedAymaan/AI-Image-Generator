9d86443436f1aae6009f42e877becea43a8bd5a9
AhmedAymaan <78112105+AhmedAymaan@users.noreply.github.com>
2024-01-27 11:51:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const api = "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";
+const inp = document.getElementById('inp')
+const images = document.querySelector('.images')
+const getImage = async () => {
+    const methods = {
+        method : "POST",
+        headers : {
+            "Content-Type" : "application/json",
+            "Authorization" : `Bearer ${api}`
+        },
+        body: JSON.stringify(
+            {
+            "prompt" : inp.value,
+            "n" : 5,
+            "size" : "512x512"
+            }
+        )
+    }
+    const res = await fetch("https://api.openai.com/v1/images/generations", methods)
+    const data = await res.json()
+    const listImages = data.data;
+    //console.log(data)
+    images.innerHTML = ''
+    listImages.map(photo => {
+        const container = document.createElement("div")
+        images.append(container)
+        const img = document.createElement("img");
+        container.append(img)
+        img.src = photo.url
+    })
+    
+}
